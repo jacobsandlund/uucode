e7ad7a7a96c05b9a83e8ceeb9bdb6851dc6ac9da
Jacob Sandlund <jacob@jacobsandlund.com>
2025-09-02 23:24:12 -0400

    fewer const in `data`

--- src/get.zig
+++ src/get.zig
@@ -53,9 +53,7 @@ fn getTable(comptime table_name: []const u8) GetTable(table_name) {
 fn data(comptime table: anytype, cp: u21) DataFor(table) {
     const stage1_idx = cp >> 8;
     const stage2_idx = cp & 0xFF;
-    const block_start = @as(usize, table.stage1[stage1_idx]) << 8;
-    const data_idx = table.stage2[block_start + stage2_idx];
-    return table.data[data_idx];
+    return table.data[table.stage2[(@as(usize, table.stage1[stage1_idx]) << 8) + stage2_idx]];
 }
 
 pub fn getPacked(comptime table_name: []const u8, cp: u21) PackedTypeOf(table_name) {
