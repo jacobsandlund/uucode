b11932d2e5577db1f5ea8db137001c1ac9f6f3c6
Jacob Sandlund <jacob@jacobsandlund.com>
2025-11-16 19:59:48 -0500

    WIP

--- src/x/config_x/wcwidth.zig
+++ src/x/config_x/wcwidth.zig
@@ -1,7 +1,19 @@
 //! The `wcwidth` is a calculation of the expected width of a codepoint in
 //! cells of a monospaced font. It is not part of the Unicode standard.
 //!
-//! See resources/wcwidth for other implementations.
+//! See resources/wcwidth for other implementations, that informed the
+//! implementation here.
+//!
+//! This implementation makes the following choices:
+//!
+//! * This wcwidth computes the width for a codepoint as it would display
+//!   standing alone without being combined with other codepoints in a grapheme
+//!   cluster. See `src/x/grapheme.zig` for the logic determining the width of
+//!   a grapheme cluster.
+//! * The returned width is never negative. C0 and C1 control characters are
+//!   treated as zero width, and generally assumed to have been stripped from
+//!   the input.
+//!
 
 const std = @import("std");
 const config = @import("config.zig");
@@ -19,12 +31,10 @@ fn compute(
     const gc = data.general_category;
     const block = data.block;
 
-    if (cp == 0) {
+    if (gc == .other_control) {
         data.wcwidth = 0;
-    } else if (gc == .other_control) {
-        data.wcwidth = -1;
-    } else if (gc == .mark_nonspacing or gc == .mark_enclosing) {
-        data.wcwidth = 0;
+        //} else if (gc == .mark_nonspacing or gc == .mark_enclosing) {
+        //    data.wcwidth = 0;
     } else if (cp == 0x00AD) { // Soft hyphen
         data.wcwidth = 1;
     } else if (cp == 0x2E3A) { // Two-em dash
@@ -57,6 +67,6 @@ pub const wcwidth = config.Extension{
     },
     .compute = &compute,
     .fields = &.{
-        .{ .name = "wcwidth", .type = i3 },
+        .{ .name = "wcwidth", .type = u3 },
     },
 };
